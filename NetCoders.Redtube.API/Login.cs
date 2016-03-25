b955bf01500a47574f8ae54b8e6bc60c28be0ba0
Felipe Leitão <fesilva@outlook.com.br>
2016-03-25 01:52:40 -0300

    aaa

---
+++ NetCoders.Redtube.API/Login.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetCoders.Redtube.API
+{
+    public class Login
+    {
+        public void Login()
+        {
+
+
+
+            return View();
+        }
+    }
+}
